383528b6dd46b89594f91e040df78ced8162a97d
Amos Thando Mpofu <219037801@student.uj.ac.za>
2023-10-03 16:25:18 +0200

    update website

--- Recygo/admin_panel/urls.py
+++ Recygo/admin_panel/urls.py
@@ -7,7 +7,7 @@ from admin_panel import views
 app_name = "admin_v2"
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
+    path("templates/admin_v2", views.dashboard, name="dashboard"),
     path("subscribed_plans/", views.subscribed_plans, name="subscribed_plans"),
     path("subscribed_plan_detail/<oid>/", views.subscribed_plan_detail, name="subscribed_plan_detail"),
     path('spending/', views.spending, name='spending'),
